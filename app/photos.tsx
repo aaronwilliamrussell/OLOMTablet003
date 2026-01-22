237ce24be906f1c9752c9ccf58e0d96e6f7aef6b
aaron.russell <aaron.russell@cna.nl.ca>
2026-01-22 16:25:55 -0330

    Photo iterator done. Images don't display yet, but we'll figure that out

--- app/photos.tsx
+++ app/photos.tsx
@@ -1,31 +1,54 @@
-import { StyleSheet, Text, View, ScrollView, Image } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import Gallery from 'react-native-awesome-gallery';
+import React from 'react';
+import * as photosList from '../assets/photos/photoData.json';
 
 const photos = () => {
     console.log("This is the photos page!")
+
+//OK...so this is going to loop through the json file and return JUST the photo for now. We'll see if it works
+//UPDATE: This doesn't work, but we'll figure out why eventually
+const givePhotos = () => {
+
+let photoArray = [];
+
+for (const key in photosList){
+  photoArray.push(photosList[key].Filename);
+}
+
+console.log(photoArray[0]);
+
+return (
+  photoArray
+)
+
+}
+
+
+
   return (
-    <View style ={pstyles.container}>
-      <Text>photos</Text>
-      <Image style={pstyles.image} source={{uri: 'https://www.heritage.nf.ca/articles/society/images/our-lady-mercy.jpg'}}></Image>
-    </View>
+    //The problem may have something to do with this. Make some comments to debug
+    <Gallery
+    data ={givePhotos()}
+    onIndexChange={(newIndex) => {
+      console.log(newIndex);
+    }}
+    >
+    </Gallery>
+
+    // <View style={styles.container}>
+    //   <Text>Photo gallery in progress!!</Text>
+    // </View>
+
   )
 }
 
 export default photos
 
-const pstyles = StyleSheet.create({
+const styles = StyleSheet.create({
     container:{
         flex:1,
         justifyContent:"center",
         alignItems:"center"
-    },
-
-    image:{
-    width:2000, 
-    height:256,
-    marginBottom:2,
-    zIndex:1,
-    shadowColor: 'rgb(0, 0, 0)',
-    shadowOffset: {width:10, height:10},
-    shadowRadius: 2},
+    }
 })
